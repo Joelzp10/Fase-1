516f9d880dd25442ccc5b3e3493edb050bab8de3
Joelzp10 <111027177+Joelzp10@users.noreply.github.com>
2022-11-02 22:47:57 -0600

    Add files via upload

---
+++ Fase 3/Farmacia_pwa-main/acerca/acerca.php
@@ -0,0 +1,22 @@
+<?php
+include("../seguridad.php");
+include_once("../central/central.php");
+include("../conexion/clsConexion.php");
+?>
+<div class="page-container">
+  <div class="main-content">
+  	<?php include('../central/cabecera.php');?>
+		<blockquote class="blockquote-default" align="center">
+			<p><strong><h3>DERECHOS DE AUTOR:<h3></strong></p>
+
+    <p align="left"><b>SISTEMA:</b></p>
+    <p align="left">Sistema de Farmacias</p>
+
+    <p align="left"><b>EMPRESA:</b></p>
+    <p align="left">UMG</p>
+
+    <p align="left"><b>DESARROLLADO POR:</b></p>
+    <p align="left"><a target="_blank">c.jimenez</a></p>
+		</blockquote>
+</div>
+</div>
